47249dd236202d0bf581c2280180592f470b33b4
tomhon <tomhon@hotmail.com>
2017-04-03 15:28:49 -0700

    reverted to k9 code

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
+// var restify = require('restify');
+
+// // function respond(req, res, next) {
+// //   res.send('hello ' + req.params.name);
+// //   next();
+// // };
+
+// // function respondbye(req, res, next) {
+// //   res.send('goodbye ' + req.params.name);
+// //   next();
+// // };
+
+// var server = restify.createServer();
+
+// server.get('/', function (req, res) { 
+//    res.send('Restify Playground');
+// }); 
+
+// // server.get('/hello/:name', respond);
+// // server.get('/goodbye/:name', respondbye);
+// // server.head('/hello/:name', respond);
+// // server.pre(restify.pre.userAgentConnection());
+
+// server.listen(8080, function() {
+//   console.log('%s listening at %s');
+// });
+
+
+
+//*************************************************** */
+
+
+
 var restify = require('restify');
-
-// function respond(req, res, next) {
-//   res.send('hello ' + req.params.name);
-//   next();
-// };
-
-// function respondbye(req, res, next) {
-//   res.send('goodbye ' + req.params.name);
-//   next();
-// };
-
 var server = restify.createServer();
 
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
+
+
+
+
+// Setup Restify Server 
+
 server.get('/', function (req, res) { 
-   res.send('Restify Playground');
+
+    res.send('K9 Production Bot Running');
 }); 
 
-// server.get('/hello/:name', respond);
-// server.get('/goodbye/:name', respondbye);
-// server.head('/hello/:name', respond);
-// server.pre(restify.pre.userAgentConnection());
 
-server.listen(8080, function() {
-  console.log('%s listening at %s');
-});

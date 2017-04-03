72d00064603eeeb2432ed7009bdc7b3ec8caec89
tomhon <tomhon@hotmail.com>
2017-04-03 16:09:17 -0700

    back to basic restify server

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ var server = restify.createServer();
 
 
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s'); 
 });
 
 
@@ -46,7 +46,7 @@ server.listen(process.env.port || 3978, function () {
 
 server.get('/', function (req, res) { 
 
-    res.send('K9 Production Bot Running');
+    res.send('Restify Playground running');
 }); 
 
 

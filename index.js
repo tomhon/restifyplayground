29922cce0fbf5f5cc0449d3ab4f65a393a5d63ae
tomhon <tomhon@hotmail.com>
2017-04-03 15:23:07 -0700

    stripped out everything

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var restify = require('restify');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-};
+// function respond(req, res, next) {
+//   res.send('hello ' + req.params.name);
+//   next();
+// };
 
-function respondbye(req, res, next) {
-  res.send('goodbye ' + req.params.name);
-  next();
-};
+// function respondbye(req, res, next) {
+//   res.send('goodbye ' + req.params.name);
+//   next();
+// };
 
 var server = restify.createServer();
 
@@ -16,8 +16,8 @@ server.get('/', function (req, res) {
    res.send('Restify Playground');
 }); 
 
-server.get('/hello/:name', respond);
-server.get('/goodbye/:name', respondbye);
+// server.get('/hello/:name', respond);
+// server.get('/goodbye/:name', respondbye);
 // server.head('/hello/:name', respond);
 // server.pre(restify.pre.userAgentConnection());
 

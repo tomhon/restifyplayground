4d17e984dc6ff02bb74530f60c52efeab58d0f35
tomhon <tomhon@hotmail.com>
2017-04-03 15:16:00 -0700

    reverted to basic server response

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ function respondbye(req, res, next) {
 }
 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.get('/goodbye/:name', respondbye);
-server.head('/hello/:name', respond);
-server.pre(restify.pre.userAgentConnection());
+server.get('/', function (req, res) { 
+   res.send('Restify Playground');
+}); 
+// server.get('/hello/:name', respond);
+// server.get('/goodbye/:name', respondbye);
+// server.head('/hello/:name', respond);
+// server.pre(restify.pre.userAgentConnection());
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);

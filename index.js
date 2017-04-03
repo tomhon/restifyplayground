fed90998fb849a2e0cf7f78d3b5be0be8082efbe
tomhon <tomhon@hotmail.com>
2017-04-03 13:53:09 -0700

    4th commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+function respondbye(req, res, next) {
+  res.send('goodbye ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.get('/goodbye/:name', respondbye);
+server.head('/hello/:name', respond);
+server.pre(restify.pre.userAgentConnection());
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});

a3a43d4912a49a030ef5dbb67d57306f571fa203
tomhon <tomhon@hotmail.com>
2017-04-03 15:21:05 -0700

    removed undeclard variables

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ var restify = require('restify');
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
-}
+};
 
 function respondbye(req, res, next) {
   res.send('goodbye ' + req.params.name);
   next();
-}
+};
 
 var server = restify.createServer();
+
 server.get('/', function (req, res) { 
    res.send('Restify Playground');
 }); 
-// server.get('/hello/:name', respond);
-// server.get('/goodbye/:name', respondbye);
+
+server.get('/hello/:name', respond);
+server.get('/goodbye/:name', respondbye);
 // server.head('/hello/:name', respond);
 // server.pre(restify.pre.userAgentConnection());
 
 server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s');
 });
